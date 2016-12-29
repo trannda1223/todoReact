ea4c90356d50a1f2f9b8ece168456b80157b9915
David <trannda@gmail.com>
2016-12-28 20:46:39 -0800

    state & e.target

--- todo.js
+++ todo.js
@@ -1,15 +1,44 @@
 class AppComponent extends React.Component {
   constructor(props){
     super(props);
-    this.state = {count: 5};
+
+    this.nextTodoId = 1;
+    this.state = {
+      filter: { showCompleted: true },
+      todos: [
+        {id: this._nextTodoId++, text: "David", isCompleted: false},
+        {id: this._nextTodoId++, text: "tran", isCompleted: true},
+        {id: this._nextTodoId++, text: "aaa", isCompleted: true},
+        {id: this._nextTodoId++, text: "bbb", isCompleted: false}
+      ]
+    };
+
+    this._onShowCompletedChanged = this._onShowCompletedChanged.bind(this);
   }
 
   render(){
+    const {filter} = this.state;
+
     return (
-      <h1 className="david">Hello World!</h1>
+      <div>
+        <h2>Todo List</h2>
+        <label>
+          Show Completed
+          <input type="checkbox" checked={filter.showCompleted} onChange={this._onShowCompletedChanged}/>
+        </label>
+      </div>
     );
+  }//render
+
+
+  _onShowCompletedChanged(e) {
+    this.setState({
+      filter: {showCompleted: e.target.checked}//e.target is like 'this'
+    })
   }
-}
+
+}//class
+
 
 ReactDOM.render(
   <AppComponent/>,
